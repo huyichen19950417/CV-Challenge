a3397d4a3c4c2baa97ef22f573d0a69ac955c5eb
Yunhai Wu <yunhai9308@gmail.com>
2018-08-10 17:15:40 +0200

    cv files
    The framework of this project

---
+++ Angabe/challenge.m
@@ -0,0 +1,26 @@
+%% Computer Vision Challenge
+
+% Groupnumber:
+group_number = 36;
+
+% Groupmembers:
+% members = {'Max Mustermann', 'Johannes Daten'};
+members = {'Yunhai Wu', 'Yichen Hu', 'Qili Zhang', 'Yinyin Lu', 'Hongjingye Zhang'};
+
+% Email-Adress (from Moodle!):
+% mail = {'ga99abc@tum.de', 'daten.hannes@tum.de'}; ????
+mail = {};
+
+%% Load images
+ 
+  
+
+%% Free Viewpoint Rendering
+% start execution timer -> tic;
+
+
+% stop execution timer -> toc;
+elapsed_time = Inf;
+
+%% Display Output
+% Display Virtual View
